21674deaa2dd22c505534c8b0af3d2a780ab49da
Biora <b30r4k@gmail.com>
2026-03-18 13:58:04 +0000

    site-front-template-2

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
+  email = '';
+
+  subscribe() {
+    if (this.email) {
+      console.log('Subscribed with email:', this.email);
+      this.email = '';
+    }
+  }
+}
